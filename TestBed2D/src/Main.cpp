3b193f5ff36f31480eb382b4cf7cb22e93d56031
Alex Ibeh <harlexibeh01@gmail.com>
2021-11-27 09:55:52 +0100

    Re-organised main entry point

--- TestBed2D/src/Main.cpp
+++ TestBed2D/src/Main.cpp
@@ -5,43 +5,57 @@
 #include "Geometry/Circle.h"
 #include "Geometry/Box.h"
 
-
 CrunchMath::Vec3 Gravity = CrunchMath::Vec3(0.0f, -9.8f, 0.0f);
 CrunchMath::World gameWorld(Gravity);
 
 std::vector<DebugRenderer::Box> Boxes;
 
+double Current_Xpos = 0.0f;
+double Current_Ypos = 0.0f;
+
+float Window_Height = 600.0f;
+float Window_Width = 800.0f;
 void FrameBufferResizeCallBack(GLFWwindow* window, int width, int height)
 {
-    glViewport(0, 0, width, height);
+    Window_Height = height;
+    Window_Width = width;
+
+    glViewport(0, 0, Window_Width, Window_Height);
 }
 
 void TrackCusorCallBack(GLFWwindow* window, double xPos, double yPos)
 {
-	CrunchMath::cmBox boxshape2;
-	boxshape2.Set((0.05f / 2.0f), (0.05f / 2.0f), 0.0f);
+    Current_Xpos = xPos;
+    Current_Ypos = yPos;
+}
 
-	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
-	{
-		float Xaxis = ((xPos + xPos) / 800.0f) - 1;
-		float Yaxis = 1 - ((yPos + yPos) / 600.0f);
+void MouseButtonCallBack(GLFWwindow* window, int button, int action, int mods)
+{
+}
 
-		DebugRenderer::Box newBox;
-		newBox.Size = CrunchMath::Vec3(0.05f, 0.05f, 0.0f); 
-		newBox.Position = CrunchMath::Vec3(Xaxis, Yaxis, 0.0f); 
+void Spawn()
+{  
+    CrunchMath::cmBox boxshape2;
+    boxshape2.Set((0.05f / 2.0f), (0.05f / 2.0f), 0.0f);
 
-		CrunchMath::Body* newbody = gameWorld.CreateBody(&boxshape2);
-		newbody->SetPosition(newBox.Position.x, newBox.Position.y, 0.0f);
-		newbody->SetOrientation(0.0f, 0.0, 0.0f, 2.0f);
-		newbody->SetVelocity(0.0f, 0.0f, 0.0f);
-		newbody->SetDamping(0.9f, 0.9f);
-		newbody->CalculateDerivedData();
-		newbody->SetMass(100);
-		newbody->SetBlockInertiaTensor((newBox.Size / 2.0f), 100);
-		newbody->SetAwake(true);
-		newBox.body = newbody;
-		Boxes.emplace_back(newBox);
-	}
+    float Xaxis = ((Current_Xpos + Current_Xpos) / Window_Width) - 1;
+    float Yaxis = 1 - ((Current_Ypos + Current_Ypos) / Window_Height);
+
+    DebugRenderer::Box newBox;
+    newBox.Size = CrunchMath::Vec3(0.05f, 0.05f, 0.0f);
+    newBox.Position = CrunchMath::Vec3(Xaxis, Yaxis, 0.0f);
+
+    CrunchMath::Body* newbody = gameWorld.CreateBody(&boxshape2);
+    newbody->SetPosition(newBox.Position.x, newBox.Position.y, 0.0f);
+    newbody->SetOrientation(0.0f, 0.0, 0.0f, 2.0f);
+    newbody->SetVelocity(0.0f, 0.0f, 0.0f);
+    newbody->SetDamping(0.9f, 0.9f);
+    newbody->CalculateDerivedData();
+    newbody->SetMass(100);
+    newbody->SetBlockInertiaTensor((newBox.Size / 2.0f), 100);
+    newbody->SetAwake(true);
+    newBox.body = newbody;
+    Boxes.emplace_back(newBox);
 }
 
 int main()
@@ -53,16 +67,18 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "CrunchMath-TestBed2D", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(Window_Width, Window_Height, "CrunchMath-TestBed2D", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, FrameBufferResizeCallBack);
     glfwSetCursorPosCallback(window, TrackCusorCallBack);
+    glfwSetMouseButtonCallback(window, MouseButtonCallBack);
     glfwSwapInterval(1);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -74,10 +90,11 @@ int main()
     float thetha = 0;
     int Count = 0;
     float Ts = (1.0f / 200.0f);
-   
-     //Circle Circle2; 
-    //Circle Circle; 
 
+    /*
+    * Circle Circle2;
+    * Circle Circle; 
+    */
 
     DebugRenderer::Box Box1;
     Box1.Size = CrunchMath::Vec3(100.0f, 0.5f, 0.0f);
@@ -103,12 +120,13 @@ int main()
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        gameWorld.Step(Ts);
+        int State = glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT);
+        if (State == GLFW_PRESS)
+            Spawn();
 
+        gameWorld.Step(Ts);
         for (int a = 0; a < Boxes.size(); a++)
-        {
             Boxes[a].Render();
-        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
